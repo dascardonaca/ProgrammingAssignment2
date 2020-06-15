010ff4910401862a511a5f16e231454d399bca84
dascardonaca <dascardonaca@unal.edu.co>
2020-06-14 19:45:27 -0500

    Solution

--- cachematrix.R
+++ cachematrix.R
@@ -38,31 +38,6 @@ cacheSolve <- function(x, ...) {
     i
 }
 
-makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-    set <- function(y) {
-        x <<- y
-        i <<- NULL
-    }
-    get <- function() x
-    setinverse <- function(solve) i <<- solve
-    getinverse <- function() i
-    list(set = set, get = get,
-         setinverse = setinverse,
-         getinverse = getinverse)
-}
-
-cacheSolve <- function(x, ...) {
-    i <- x$getinverse()
-    if(!is.null(i)) {
-        message("getting cached data")
-        return(i)
-    }
-    data <- x$get()
-    i <- solve(data, ...)
-    x$setinverse(i)
-    i
-}
 
 '''
 hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
